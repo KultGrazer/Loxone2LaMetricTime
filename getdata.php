1e26f5e6e3a767644254aee7a2ee2db90c1e407c
KultGrazer <bernhard@pikl.co.at>
2017-05-21 20:10:53 +0200

    Update getdata.php

--- getdata.php
+++ getdata.php
@@ -1,9 +1,9 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('allow_url_fopen', '1');
+ini_set('display_errors', 1);                   // Display Errors if anything went wrong. 
+ini_set('allow_url_fopen', '1');                // Is needed to get Date from http://
 
-$username = "admin"; 								//Loxone Username
-$password = "admin"; 								//Loxone Password
+$username = "admin"; 						            		//Loxone Username
+$password = "admin"; 							             	//Loxone Password
 $url1 = 'YOURIP:PORT/dev/sps/io/ATemp/state';		// Link to your State without http://
 
 $xml1= simplexml_load_file('http://'. $username .':'. $password .'@'. $url1 );
